Fixing minor bug in distance calculation and renaming to identity_like

sympa/manifolds/symmetric_manifold.py
@@ -36,7 +36,7 @@ class SymmetricManifold(Manifold, ABC):
         :return: distance between x and y in the UHSM
         """
         # with Z1 = X + iY, define Z3 = sqrt(Y)^-1 (Z2 - X) sqrt(Y)^-1
-        real_z1, imag_z1 = sm.real(z1), sm.imag(z2)
+        real_z1, imag_z1 = sm.real(z1), sm.imag(z1)
         inv_sqrt_imag_z1 = sm.matrix_sqrt(imag_z1).inverse()
         inv_sqrt_imag_z1 = sm.stick(inv_sqrt_imag_z1, torch.zeros_like(inv_sqrt_imag_z1))
         z2_minus_real_z1 = sm.subtract(z2, sm.stick(real_z1, torch.zeros_like(real_z1)))

sympa/math/symmetric_math.py
@@ -221,7 +221,7 @@ def identity(dims: int):
     return torch.stack((real, imag))
 
 
-def identity_from_tensor(z: torch.Tensor):
+def identity_like(z: torch.Tensor):
     """
     Return an identity of the shape of z, with the same type and device.
     :param z: b x 2 x n x n

sympa/utils.py
@@ -42,4 +42,4 @@ def row_sort(x, indexes):
 
 
 def assert_all_close(a, b, factor=1):
-    return torch.all((a - b).abs() < EPS[a.dtype] * factor)
+    return torch.all((a - b).abs() < EPS[torch.float32] * factor)