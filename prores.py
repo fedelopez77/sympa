ed350aa725612ce7b2ee50fb8dc02a47a28a8c25
Federico Lopez <fedelopez77@gmail.com>
2020-09-16 15:56:50 +0200

    Adding script to process file with many results

---
+++ prores.py
@@ -0,0 +1,28 @@
+
+import argparse
+import pandas as pd
+from pathlib import Path
+RUN_ID = "run_id"
+DISTORTION = "distortion"
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="process_results.py")
+    parser.add_argument("--file", required=True, help="Path to results file")
+    args = parser.parse_args()
+
+    data = pd.read_csv(args.file)
+
+    # remove run_number from run
+    data[RUN_ID] = data[RUN_ID].map(lambda x: x[:-2])
+
+    grouped = data.groupby(RUN_ID)
+    means = grouped.mean()
+    stds = grouped.std().rename(lambda x: x + "_std", axis="columns")
+
+    means_and_stds = means.join(stds)
+    means_and_stds = means_and_stds.sort_values(by=[DISTORTION], ascending=False)
+
+    path = Path(args.file)
+    new_path = path.parent / f"AA-{path.name[:-4]}.csv"
+    means_and_stds.to_csv(str(new_path))
