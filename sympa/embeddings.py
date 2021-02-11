cd6b3c94e481121838b59456343099ca942ffc7b
Federico Lopez <fedelopez77@gmail.com>
2021-02-11 11:06:30 +0100

    Removing files with SPD, and just using the latest update of geoopt (v0.3.1)

--- sympa/embeddings.py
+++ sympa/embeddings.py
@@ -3,17 +3,18 @@ import torch
 import torch.nn as nn
 from sympa import config
 import geoopt as gt
+from geoopt.manifolds.symmetric_positive_definite import SymmetricPositiveDefinite
 from sympa.manifolds.metric import Metric
-from sympa.manifolds import BoundedDomainManifold, UpperHalfManifold, SymmetricPositiveDefinite
+from sympa.manifolds import BoundedDomainManifold, UpperHalfManifold
 
 
 class Embeddings(nn.Module, abc.ABC):
     """Abstract Embedding layer that operates with embeddings as a Manifold parameter"""
 
-    def __init__(self, num_embeddings, embedding_dim, manifold, _embeds):
+    def __init__(self, num_embeddings, dims, manifold, _embeds):
         """
         :param num_embeddings: number of elements in the table
-        :param embedding_dim: dimensionality of embeddings.
+        :param dims: dimensionality of embeddings.
         If it is vector embeddings, it is the amount of dimensions in the vector.
         If it is Matrix embeddings, it is the last dimension on the matrix
         :param manifold: Instance of Geoopt Manifold
@@ -21,7 +22,7 @@ class Embeddings(nn.Module, abc.ABC):
         """
         super().__init__()
         self.num_embeddings = num_embeddings
-        self.embedding_dim = embedding_dim
+        self.embedding_dim = dims
         self.manifold = manifold
         self.embeds = gt.ManifoldParameter(_embeds, manifold=self.manifold)
 
@@ -50,21 +51,28 @@ class Embeddings(nn.Module, abc.ABC):
         pass
 
 
-class ComplexSymmetricMatrixEmbeddings(Embeddings):
+class MatrixEmbeddings(Embeddings):
 
-    def __init__(self, num_embeddings, embedding_dim, manifold):
+    def __init__(self, num_embeddings, dims, manifold):
         """
-        Represents Embeddings of Complex Symemtric Matrices.
-        The shape of the embedding table is: num_embeddings x 2 x embedding_dim x embedding_dim
+        Represents Embeddings of Matrices.
+        The shape of the embedding table for complex matrices is: num_embeddings x 2 x dims x dims
         The second dimension is 2, since the element 0 represents the real part,
         and the element 1 represents the imaginary part of the matrices.
 
+        For regular matrices (SPD in particular) the shape is: num_embeddings x embedding_dim x embedding_dim
+
         :param num_embeddings: number of elements in the table
-        :param embedding_dim: dimensionality of matrix embeddings.
-        :param manifold: UpperHalfManifold or BoundedDomainManifold
+        :param dims: dimensionality of matrix embeddings.
+        :param manifold: SymmetricPositiveDefinite, UpperHalfManifold, BoundedDomainManifold
         """
-        _embeds = manifold.random(num_embeddings, embedding_dim, from_=-config.INIT_EPS, to=config.INIT_EPS)
-        super().__init__(num_embeddings, embedding_dim, manifold, _embeds)
+        _embeds = manifold.random(num_embeddings, dims, dims, from_=-config.INIT_EPS, to=config.INIT_EPS)
+
+        if isinstance(manifold, SymmetricPositiveDefinite):   # scales the SPD embeddings
+            _embeds *= config.INIT_EPS
+            _embeds += torch.diag_embed(torch.ones(num_embeddings, dims))
+
+        super().__init__(num_embeddings, dims, manifold, _embeds)
 
     def norm(self):
         points = self.embeds.data
@@ -73,10 +81,10 @@ class ComplexSymmetricMatrixEmbeddings(Embeddings):
 
 
 class VectorEmbeddings(Embeddings):
-    def __init__(self, num_embeddings, embedding_dim, manifold):
+    def __init__(self, num_embeddings, dims, manifold):
         init_eps = config.INIT_EPS
-        _embeds = torch.Tensor(num_embeddings, embedding_dim).uniform_(-init_eps, init_eps)
-        super().__init__(num_embeddings, embedding_dim, manifold, _embeds)
+        _embeds = torch.Tensor(num_embeddings, dims).uniform_(-init_eps, init_eps)
+        super().__init__(num_embeddings, dims, manifold, _embeds)
         self.proj_embeds()
 
     def norm(self):
@@ -99,7 +107,7 @@ def get_bounded_fone_manifold(dims): return BoundedDomainManifold(dim=dims, metr
 def get_bounded_finf_manifold(dims): return BoundedDomainManifold(dim=dims, metric=Metric.FINSLER_INFINITY.value)
 def get_bounded_fmin_manifold(dims): return BoundedDomainManifold(dim=dims, metric=Metric.FINSLER_MINIMUM.value)
 def get_bounded_wsum_manifold(dims): return BoundedDomainManifold(dim=dims, metric=Metric.WEIGHTED_SUM.value)
-def get_spd(dims): return SymmetricPositiveDefinite(dim=dims)
+def get_spd(dims): return SymmetricPositiveDefinite()
 
 
 def get_prod_hysph_manifold(dims):
@@ -151,12 +159,12 @@ class EmbeddingsBuilder:
     @classmethod
     def get_embeddings(cls, name: str, num_points: int, dims: int, manifold):
         embed_table = cls._get_table(name)
-        return embed_table(num_embeddings=num_points, embedding_dim=dims, manifold=manifold)
+        return embed_table(num_embeddings=num_points, dims=dims, manifold=manifold)
 
     @classmethod
     def _get_table(cls, model_name: str):
         if "upper" in model_name or "bounded" in model_name or "spd" in model_name:
-            return ComplexSymmetricMatrixEmbeddings
+            return MatrixEmbeddings
         if model_name in {"euclidean", "poincare", "lorentz", "sphere", "prod-hysph", "prod-hyhy", "prod-hyeu"}:
             return VectorEmbeddings
         raise ValueError(f"Unrecognized embedding model: {model_name}")
--- sympa/manifolds/__init__.py
+++ sympa/manifolds/__init__.py
@@ -1,4 +1,3 @@
 from .symmetric_manifold import SymmetricManifold
 from .bounded_domain import BoundedDomainManifold
 from .upper_half import UpperHalfManifold
-from .spd import SymmetricPositiveDefinite
